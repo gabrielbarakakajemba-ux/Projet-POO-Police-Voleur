5dff68819c959453ab1e29b9c72325e395f485f4
Gabriel kajemba <gabrielbarakakajemba@gmail.com>
2026-03-14 12:09:00 +0400

    Modification des petits en arc.

--- projet POO/Police_voleur_game/src/main/java/com/mycompany/police_voleur_game/Plateau.java
+++ projet POO/Police_voleur_game/src/main/java/com/mycompany/police_voleur_game/Plateau.java
@@ -14,7 +14,7 @@ import java.awt.*;
 public class Plateau extends JFrame {
     public Plateau() {
         setTitle("Police Voleur");
-        setSize(600, 600);
+        setSize(800, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         add(new BoardPanel());
@@ -31,7 +31,7 @@ public class Plateau extends JFrame {
             int height = getHeight();
             int centerX = width / 2;
             int centerY = height / 2;
-            int radius = 200;
+            int radius = 300;
             
            
             g2d.drawOval(centerX - radius, centerY - radius, radius * 2, radius * 2);
@@ -40,27 +40,30 @@ public class Plateau extends JFrame {
             g2d.drawLine(centerX - radius, centerY, centerX + radius, centerY);
             g2d.drawLine(centerX, centerY - radius, centerX, centerY + radius);
 
-            int nodeRadius = 15;
+            int nodeRadius = 40;
             drawNode(g2d, centerX, centerY, nodeRadius);
-            drawNode(g2d, centerX, centerY - radius, nodeRadius);
-            drawNode(g2d, centerX, centerY + radius, nodeRadius);
-            drawNode(g2d, centerX - radius, centerY, nodeRadius);
-            drawNode(g2d, centerX + radius, centerY, nodeRadius);
+            drawNodeArc(g2d, centerX, centerY - radius, nodeRadius);
+            
         }
         
         private void drawNode(Graphics2D g, int x, int y, int r) {
-            g.setColor(Color.WHITE);
-            g.fillOval(x - r, y - r, r * 2, r * 2);
             g.setColor(Color.BLACK);
             g.drawOval(x - r, y - r, r * 2, r * 2);
         }
-    }
-    
+        
+        private void drawNodeArc(Graphics2D g, int x, int y, int r) {
+           g.setColor(Color.BLACK);
+           int startAngle = 180;
+           int arcAngle = 180;
+           
+           g.drawArc(x - r,  y - r, r * 2, r * 2,  arcAngle, startAngle);
+        } 
+
+    }   
+        
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(() -> {
             new Plateau().setVisible(true);
         });
     }
 }
-
-
